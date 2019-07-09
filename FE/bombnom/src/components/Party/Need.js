22024db8cad2b07c1dfd98a0fbefc7692cf238f9
Trevor Fehrman <trevorfehrman@gmail.com>
2019-07-09 01:16:48 -0700

    stylin dialin

--- FE/bombnom/src/components/Party/Need.js
+++ FE/bombnom/src/components/Party/Need.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
+import Icon from '@material-ui/core/Icon';
+import clsx from 'clsx';
 
 const useStyles = makeStyles({
   need: ({ need, fulfilled }) => ({
@@ -20,6 +22,7 @@ const useStyles = makeStyles({
 
     position: 'relative',
     transition: 'all .2s ease',
+    cursor: 'pointer',
 
     '&:hover': {
       zIndex: 10,
@@ -92,7 +95,9 @@ const useStyles = makeStyles({
       whiteSpace: 'nowrap',
       textOverflow: 'ellipsis',
       overflow: 'hidden',
-      fontStyle: need.defined ? null : 'italic'
+      fontStyle: need.defined ? null : 'italic',
+      position: 'relative',
+      top: '8px'
     }
   }),
   notes: {
@@ -102,16 +107,48 @@ const useStyles = makeStyles({
       color: 'white'
     }
   },
-  bringer: {
+  bringer: ({ need, fulfilled }) => ({
     height: '17%',
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingRight: '2rem',
-    borderBottom: '1px solid gray',
+    borderBottom: '1px solid #e6e6e6',
+    transition: 'all .2s ease-out',
+    '&:hover': {
+      backgroundColor:
+        need.priority === 1
+          ? '#EF535088'
+          : need.priority === 2
+          ? '#4caf5088'
+          : need.priority === 3
+          ? '#9c27b088'
+          : need.priority === 4
+          ? '#2196f388'
+          : need.priority === 5
+          ? '#00968888'
+          : '#607d8b88',
+      borderBottom: 'none'
+    },
     '& img': {
-      height: '100%',
+      height: '100%'
     }
+  }),
+  icon: {
+    fontSize: '2.5rem'
+  },
+  stillNeed: {
+    position: 'absolute',
+    top: 0,
+    right: '1rem',
+    zIndex: 100,
+    color: 'white',
+    backgroundColor: 'rgba(0, 0, 0, 0.53)',
+    // borderBottomRightRadius: '6px',
+    // borderBottomLeftRadius: '6px',
+    padding: '0 4px',
+    fontWeight: 'bold',
+    fontSize: '1rem'
   }
 });
 
@@ -120,15 +157,22 @@ const Need = ({ need, fulfilled }) => {
 
   return (
     <div key={need.id} className={classes.need}>
+      {need.quantity - need.quantity_fulfilled !== 0 && need.quantity_fulfilled ? (
+        <Typography variant='caption' className={classes.stillNeed}>
+          Still Need
+        </Typography>
+      ) : null}
       <div className={classes.needContent}>
         <strong className={classes.needName}>
           <Typography gutterBottom={true} variant='h5'>
             {need.need}
           </Typography>
           <Typography gutterBottom={true} variant='h6'>
-            {need.quantity - need.quantity_fulfilled > 0
-              ? need.quantity - need.quantity_fulfilled
-              : '✔'}{' '}
+            {need.quantity - need.quantity_fulfilled > 0 ? (
+              need.quantity - need.quantity_fulfilled
+            ) : (
+              <Icon color='secondary' className={clsx(classes.icon, 'fas fa-bomb')} />
+            )}{' '}
             {need.quantity - need.quantity_fulfilled > 0 ? need.quantity_unit : null}
           </Typography>
         </strong>
@@ -140,11 +184,9 @@ const Need = ({ need, fulfilled }) => {
         {need.bringers.length > 0 &&
           need.bringers.map(bringer => {
             return (
-              <div className={classes.bringer}>
+              <div className={classes.bringer} key={bringer.email}>
                 <img alt={bringer.username} src={bringer.img_url} />
-                <Typography variant='body1' key={bringer.email}>
-                  {bringer.username}
-                </Typography>
+                <Typography variant='body1'>{bringer.username}</Typography>
                 <Typography variant='body1'>{bringer.quantity_fulfilled}</Typography>
               </div>
             );
